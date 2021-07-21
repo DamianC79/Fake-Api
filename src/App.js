b306d3d1325466da345345e5773733c0872bcb8a
DamianC79 <damianeldiez@gmail.com>
2021-07-21 13:06:16 -0300

    commit de prueba

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import styled from '@emotion/styled';
+import Books from './Books';
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+  const [books,obtenerBooks]=useState({});
+
+  const consultarAPI = async () => {
+    const api = await fetch('https://fakerapi.it/api/v1/books?_quantity=1');
+    const books = await api.json();
+    obtenerBooks(books); 
+  }
+    
+  const Contenedor = styled.div`
+  display: flex;
+  align-items: center;
+  padding-top:5rem;
+  flex-direction:column;
+`;
+    
+    return (
+      <Contenedor >
+        <Books
+          books ={books}
+        />
+        <button 
+          onClick={ consultarAPI }
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          Obtener Resultados
+        </button>
+      </Contenedor>
+    );
 }
 
 export default App;
---
+++ src/Books.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Books=({books})=>{
+    return(
+        <div>
+        <h1>{books.title}</h1>
+        </div>
+
+    );
+}
+
+export default Books;
