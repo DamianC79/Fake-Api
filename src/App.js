67b450bb07bf5e4da18994aab1706f57460e9a54
DamianC79 <damianeldiez@gmail.com>
2021-07-23 22:30:27 -0300

    replanteamiento de App

--- src/App.js
+++ src/App.js
@@ -1,41 +1,26 @@
 import React, {useState} from 'react';
-import './App.css'
 import Books from './Books.js';
 import Users from './Users.js';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 
-  
+
 
 function App() {
 
-  const [books,obtenerBooks]=useState({});
+  
 
-  const consultarAPI = async () => {
-    const api = await fetch('https://fakerapi.it/api/v1/books?_quantity=5');
-    const books = await api.json();
-    obtenerBooks(books.data); 
-    console.log(api)
-    console.log(books)
-    
-  }
-    
- 
-    
-  console.log(books);
-  return (
-    <>
+
+
+    return (
+      <>
         <Router>
 
           <Switch>
 
-            <Route path="/users">
-              <h1>USERS</h1>    
-            </Route>
+            <Route exact path="/" component={Books}/>
 
-            <Route path="/">
-              <h1>BOOKS</h1>
-            </Route>
+            <Route exact path="/users" component={Users}/>
 
           </Switch>
 
@@ -43,12 +28,6 @@ function App() {
 
       <div >
 
-        <Books 
-        books={books}
-        />
-        <button className="button" onClick={ consultarAPI }
-          > Buscar 
-        </button>
 
       </div>
       </>
