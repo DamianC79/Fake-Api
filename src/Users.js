94b737c702229fa4527ef6994f5c5a07eced9bd8
DamianC79 <damianeldiez@gmail.com>
2021-07-23 23:12:34 -0300

    correccion de errores

--- src/Users.js
+++ src/Users.js
@@ -1,12 +1,38 @@
-import React from 'react';
- 
+import React, {useEffect, useState} from 'react';
+import { getUsers } from './api'; 
 
-function Users()  {
-    return ( 
-        
-                    <h1>Usuarios</h1>
-                
+const Users = () => {
+    const [users,obtenerUsers]=useState([]);
+
+    useEffect(() => {
+      getUsers
+      .then(data =>{
+        console.log(users)
+        obtenerUsers(data)
+      })   
+    }, [])
+
+    return(
+            <>
+            <h1>Users</h1>
+            <table>
+                <tbody>
+                    {
+                        users.map(user => (
+                            <tr key={user.ip}>
+                                <td>{user.firstname}</td>
+                                <td>{user.lastname}</td>
+                                <td>{user.username}</td>
+                                <td>{user.email}</td>
+                                <td>{user.password}</td>
+                                <td><img src={user.image} width="50px" height="50px"/></td>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </table>      
+            </>
      );
 }
- 
+
 export default Users;
