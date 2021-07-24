950c1b877174f0871a14c758b15934f6f4e2633d
DamianC79 <damianeldiez@gmail.com>
2021-07-23 22:42:04 -0300

    componente Books terminado

--- src/Books.js
+++ src/Books.js
@@ -1,14 +1,50 @@
-import React from 'react';
-import PropTypes from 'prop-types'
+import React,{useEffect, useState} from 'react';
+import PropTypes from 'prop-types';
 
-const Books=({books})=>{
+const Books=()=>{
+
+    const [books,obtenerBooks]=useState([]);
+
+    const consultarAPI = async () => {
+      const api = await fetch('https://fakerapi.it/api/v1/books?_quantity=5');
+      const books = await api.json();
+      obtenerBooks(books.data);
+
+      console.log(books.data)
+
+    }
+
+    useEffect(() => {
+        consultarAPI()
+        
+    }, [])
+
     return(
-        <div></div>
-            
+
+        <table>
+            <tbody>
+                {
+                    books.map(book => (
+                        <>
+                        <tr key={book.isbn}>
+                            <td>{book.author}</td>
+                            <td>{book.genre}</td>
+                            <td>{book.published}</td>
+                            <td>{book.publisher}</td>
+                            <td>{book.title}</td>
+                            <td><img src={book.image} width="50px" height="50px"/></td>
+                        </tr>
+                        </>
+                    ))
+                }
+            </tbody>
+        </table>
+
     );
 }
-Books.propTypes={
-    book:PropTypes.array.isRequired
+
+Books.propTypes = {
+    books: PropTypes.array.isRequired
 }
 
 export default Books;
